9cd6c8f48e0f1bd52a595576eeea265bd066fc6b
Richard Wellerson <richardwellerson@gmail.com>
2019-12-13 16:57:53 -0300

    Iniciando o projeto. Last pronto!

--- tests/last.spec.js
+++ tests/last.spec.js
@@ -2,6 +2,15 @@ const last = require('../src/last');
 
 // A função deve receber uma quantidade qualquer de parâmetros
 // A função deve retornar o último elemento do parâmetro passado
-// Se a função receber uma lista, deve retornar o último elemento
-// Se a função receber uma string, deve retornar o último caractere
-// Se a função receber vários parâmetros, deve retornar o último parâmetro
+
+test('Se a função receber uma lista, deve retornar o último elemento', () => {
+  expect(last([1, 4, 7, 9])).toBe(9)
+});
+
+test('Se a função receber uma string, deve retornar o último caractere', () => {
+  expect(last('trybe')).toBe('e')
+})
+
+test('Se a função receber vários parâmetros, deve retornar o último parâmetro', () => {
+  expect(last('Turisas','Sabaton','Arch Enemy')).toBe('Arch Enemy')
+})
