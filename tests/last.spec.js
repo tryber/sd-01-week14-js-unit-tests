2610ed072bafe9b7dadde928d083cfb7063d5ffe
Henrique Eyer <henriqueeyer@outlook.com>
2019-12-13 15:30:00 -0300

    Feito o primeiro exercicio

--- tests/last.spec.js
+++ tests/last.spec.js
@@ -5,3 +5,23 @@ const last = require('../src/last');
 // Se a função receber uma lista, deve retornar o último elemento
 // Se a função receber uma string, deve retornar o último caractere
 // Se a função receber vários parâmetros, deve retornar o último parâmetro
+
+describe('#last', () => {
+  it('A função deve retornar o último elemento do parâmetro passado', () => {
+    expect(last(arguments)).toBe(arguments[arguments.length - 1]);
+  });
+
+  it('Se a função receber uma lista, deve retornar o último elemento', () => {
+    expect(last([1,2,3,4,5])).toBe(5);
+  });
+
+  it('Se a função receber uma string, deve retornar o último caractere', () => {
+    expect(last('wilson')).toBe('n');
+    expect(last('wilson')).toBe('n');
+  });
+
+  it('Se a função receber vários parâmetros, deve retornar o último parâmetro', () => {
+    expect(last(['ola','wilson'],'bobo',true,4,false)).toBe(false);
+    expect(last(false)).toBe(false);
+  });
+});
