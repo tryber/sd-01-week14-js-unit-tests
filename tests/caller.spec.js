6910abb1023549994dd0a2083ddaabedb2b792de
Henrique Eyer <henriqueeyer@outlook.com>
2019-12-13 17:38:12 -0300

    7 teste feito

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -5,3 +5,18 @@ const caller = require('../src/caller');
 // O segundo e terceiro parâmetros podem ser de qualquer tipo
 // A função deve executar a função (primeiro parâmetro) com os outros parâmetros recebidos
 // e retornar o resultado
+
+const fn =(a,b)=>{
+  return a + b;
+}
+
+describe('#caller', () => {
+  it('A soma precisa resultar em 5, 2+3', () => {
+    expect(caller(fn, 2, 3)).toBe(5);
+  });
+
+  it('osparametros sao tutus e ronaldo, precisa sair tutusronaldo', () => {
+    expect(caller(fn, 'tutus', 'ronaldo')).toBe('tutusronaldo');
+  });
+});
+
