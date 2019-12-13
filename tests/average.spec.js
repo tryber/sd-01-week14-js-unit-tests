15fdf315a420591c97953539785707d33964be62
dougfunny1983 <douglaseduardo220483@gmail.com>
2019-12-13 17:46:47 -0300

    terminando o caller

--- tests/average.spec.js
+++ tests/average.spec.js
@@ -5,7 +5,6 @@ const average = require('../src/average');
 // Caso a função receba algum valor não número, o valor undefined deve ser retornado
 
 describe('#average', () => {
-  
   it('expect average of 3, 4, 5 should be 4', () => {
     expect(average(3, 4, 5)).toBe(4);
   });
--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -5,3 +5,10 @@ const caller = require('../src/caller');
 // O segundo e terceiro parâmetros podem ser de qualquer tipo
 // A função deve executar a função (primeiro parâmetro) com os outros parâmetros recebidos
 // e retornar o resultado
+const fn = (a, b) => a * b
+
+describe('#caller', () => {
+  it('expect caller of fn, 4, 5 should be fn(4,5)', () => {
+    expect(caller(fn, 2, 5)).toBe(10);
+  });
+});
