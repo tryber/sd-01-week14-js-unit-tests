a590fd6a23b988fa96b2df4b6e4c5205c7b076c4
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-12-13 15:14:14 -0300

    Iniciando Trabalhos

--- src/average.js
+++ src/average.js
@@ -1,5 +1,5 @@
 /*
-  A função average recebe uma lista de parâmetros (tamanho variável)
+função average recebe uma lista de parâmetros (tamanho variável)
   e retorna a média dos valores recebidos
   Caso a função receba algum valor não número, o valor undefined deve ser retornado
 */
