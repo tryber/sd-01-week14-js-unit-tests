b420c098029b17ee337983d3cb7138207c57594c
Douglas Eduardo <douglaseduardo220483@gmail.com>
2019-12-30 15:30:20 -0300

    refatoração do codigo para passar no cc

--- src/printBinary.js
+++ src/printBinary.js
@@ -2,27 +2,73 @@
   A função printBinary recebe um parâmetro inteiro ${number}
   e retorna uma lista com todos os números binários existentes com a quantidade ${number} de dígitos
  */
-
-function printBinary(number) {
+function printMaiorQue1(number) {
   const lista = []
-  if (!Number.isInteger(number) || number < 1) {
-    return lista;
-  }
-  else {
-    for (let index = 0; index <= number + 1; index++) {
-      if (index < 2) {
-        lista.push('0' + (index >>> 0).toString(2))
-      } else {
-        lista.push((index >>> 0).toString(2))
-      }
+  for (let index = 0; index <= number + 1; index++) {
+    if (index < 2) {
+      lista.push('0' + (index >>> 0).toString(2))
+    } else {
+      lista.push((index >>> 0).toString(2))
     }
-    return lista;
   }
-
+  return lista
 }
 
+function printiguala1(number) {
+  const lista = []
+  for (let index = 0; index < number + 1; index++) {
+    lista.push((index >>> 0).toString(2))
+  }
+  return lista;
+}
+
+function printBinary(value) {
+  let resposta = []
+  const cases = {
+    0: resposta,
+    'teste': resposta,
+    1: printiguala1(value),
+    2: printMaiorQue1(value),
+    3: printMaiorQue1(value),
+  };
+
+  return cases[value];
+}
+
+// let lista;
+// switch (value) {
+//   case value === 1:
+//     console.log(printiguala1(value));
+//     lista = printiguala1(value)
+//     break;
+//   case value > 1:
+//     console.log(printMaiorQue1(value));
+//     lista = printMaiorQue1(value)
+//     break;
+//   // default:
+//   //   lista = []
+// }
+
+// // if (!Number.isInteger(number) || number < 1) {
+// //   return lista;
+// // } else if (number === 1) {
+// //   for (let index = 0; index < number + 1; index++) {
+// //     lista.push((index >>> 0).toString(2))
+// //   }
+// //   return lista;
+// // }
+// // else {
+// //   for (let index = 0; index <= number + 1; index++) {
+// //     if (index < 2) {
+// //       lista.push('0' + (index >>> 0).toString(2))
+// //     } else {
+// //       lista.push((index >>> 0).toString(2))
+// //     }
+// //   }
+// // }
+
 // console.log('............................');
-// console.log(printBinary('teste'));
+// console.log(printBinary(0));
 // console.log('............................');
 
 module.exports = printBinary;
