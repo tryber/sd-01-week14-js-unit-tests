c56deecaff2c81c6dc3aeb283b615a14d074f4f4
richardwellerson <richardwellerson@gmail.com>
2020-01-04 01:45:07 -0300

    Code Climate

--- tests/average.spec.js
+++ tests/average.spec.js
@@ -10,14 +10,14 @@ describe('#average', () => {
   });
   it('expect average of 16, 32, 64, 128, 256, 512 should be 168', () => {
     expect(average(16, 32, 64, 128, 256, 512)).toBe(168);
-  })
+  });
   it('expect average of 333, 666, 999, 1332 should be 832.5', () => {
     expect(average(333, 666, 999, 1332)).toBe(832.5);
-  })
+  });
   it('expect average of 396, 113 should be 254.5', () => {
     expect(average(396, 113)).toBe(254.5);
-  })
+  });
   it('expect function returns undefined for params 1995, idade, 2020, 24, anos', () => {
     expect(average(1995, 'idade', 2020, 24, 'anos')).toBe(undefined)
-  })
+  });
 });
