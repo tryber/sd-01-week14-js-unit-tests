0022ecd599a68f952c58dde5e2a7fabe8ed85ade
richardwellerson <richardwellerson@gmail.com>
2020-01-04 05:52:17 -0300

    Readicionando testes para o arquivo printBinary.spex.js

--- tests/printBinary.spec.js
+++ tests/printBinary.spec.js
@@ -9,12 +9,16 @@ describe('#printBinary', () => {
   it('should print binary numbers until 1', () => {
     expect(printBinary(1)).toBe(['0', '1']);
   });
-
   it('should print binary numbers until 2', () => {
     expect(printBinary(2)).toBe(['00', '01', '10', '11']);
   });
-
   it('should print binary numbers until 3', () => {
     expect(printBinary(3)).toBe(['00', '01', '10', '11', '100', '101', '110', '111']);
+  });
+  it('should print a empty array because have 0 in param', () => {
+    expect(printBinary(0)).toBe([]);
+  });
+  it('should print a empty array because have string in param', () => {
+    expect(printBinary('a')).toBe([]);
   });
 });
