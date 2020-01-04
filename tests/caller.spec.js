6cf1d1b37f077e2379ef9e5778722f0b15077144
richardwellerson <richardwellerson@gmail.com>
2020-01-04 02:50:35 -0300

    Funçao de testes feita

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -1,7 +1,26 @@
 const caller = require('../src/caller');
+const addition = (a, b) => a + b;
+const subtraction = (a, b) => a - b;
+const division = (a, b) => a / b;
+const multiplication = (a, b) => a * b;
 
 // A função deve receber 3 parâmetros
 // O primeiro parâmetro deve ser uma função
 // O segundo e terceiro parâmetros podem ser de qualquer tipo
 // A função deve executar a função (primeiro parâmetro) com os outros parâmetros recebidos
 // e retornar o resultado
+
+describe('#caller', () => {
+  it('receives a function first and makes a addition', () => {
+    expect(caller(addition, 5, 6)).toBe(11);
+  });
+  it('receives a function first and makes a subtraction', () => {
+    expect(caller(subtraction, 2020, 1995)).toBe(25);
+  });
+  it('receives a function first and makes a division', () => {
+    expect(caller(division, 999, 333)).toBe(3);
+  });
+  it('receives a function first and makes a multiplication', () => {
+    expect(caller(multiplication, 32, 94)).toBe(3008);
+  });
+})
