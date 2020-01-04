16b5a797e6bd339c2d0dfc14f1e50f003b2e5d79
richardwellerson <richardwellerson@gmail.com>
2020-01-04 01:28:19 -0300

    Implementado mais testes no arquivo average.spec.js

--- tests/average.spec.js
+++ tests/average.spec.js
@@ -6,6 +6,15 @@ const average = require('../src/average');
 
 describe('#average', () => {
   it('expect average of 3, 4, 5 should be 4', () => {
-    expect(average(3, 4, 5).toBe(4));
+    expect(average(3, 4, 5)).toBe(4);
   });
+  it('expect average of 16, 32, 64, 128, 256, 512 should be 168', () => {
+    expect(average(16, 32, 64, 128, 256, 512)).toBe(168);
+  })
+  it('expect average of 333, 666, 999, 1332 should be 832.5', () => {
+    expect(average(333, 666, 999, 1332)).toBe(832.5);
+  })
+  it('expect average of 396, 113 should be 254.5', () => {
+    expect(average(396, 113)).toBe(254.5);
+  })
 });
