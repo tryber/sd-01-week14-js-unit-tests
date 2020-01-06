08cd5878ba039bddc232c256518f6622f8cb6879
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-01-06 11:53:00 -0200

    Code Climate 0.6

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -5,7 +5,7 @@ const assert = require('assert');
 // A função recebe um inteiro e retorna a lista com os fatores desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
 
-assert.deepStrictEqual(primeFactors(0) , []);
+assert.deepStrictEqual(primeFactors(0) ,[]);
 assert.deepStrictEqual(primeFactors(2), [2]);
 assert.deepStrictEqual(primeFactors(4), [2, 3]);
 assert.deepStrictEqual(primeFactors(10), [2, 3, 5, 7]);
