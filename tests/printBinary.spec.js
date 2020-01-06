287ee4117c84a94b7d471ac8eb2c8508118dfa24
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-01-06 12:02:03 -0200

    Code Climate 0.9

--- tests/printBinary.spec.js
+++ tests/printBinary.spec.js
@@ -9,19 +9,15 @@ describe('#printBinary', () => {
   it('should print empty array', () => {
     expect(printBinary(0)).toStrictEqual([]);
   });
-
   it('should print empty array', () => {
     expect(printBinary(undefined)).toStrictEqual([]);
   });
-
   it('should print binary numbers until 1', () => {
     expect(printBinary(1)).toStrictEqual(['0', '1']);
   });
-
   it('should print binary numbers until 2', () => {
     expect(printBinary(2)).toStrictEqual(['00', '01', '10', '11']);
   });
-
   it('should print binary numbers until 3', () => {
     expect(printBinary(3)).toStrictEqual(['100', '101', '000', '001', '010', '011', '110', '111']);
   });
