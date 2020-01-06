16b91548c9b9cdd586ab6afc8743c229037e8b8b
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-01-06 11:44:47 -0200

    Code Climate 0.4

--- src/printBinary.js
+++ src/printBinary.js
@@ -7,13 +7,13 @@ function printBinary(number) {
   const digBinary = ['0','1'];
   const previosArray = ['0','1'];
   if (number === 0 || typeof number !== 'number') {
-    return []
+    return [];
   }
   for(let i = 0; i < number; i++) {
-    digBinary.forEach((dig) => previosArray.forEach((digt) => previosArray.push(dig + digt)))
+    digBinary.forEach((dig) => previosArray.forEach((digt) => previosArray.push(dig + digt)));
   }
-  arr = previosArray.filter((elm) => elm.length === number)
-  return arr.filter((este, i) => arr.indexOf(este) === i)
+  arr = previosArray.filter((elm) => elm.length === number);
+  return arr.filter((este, i) => arr.indexOf(este) === i);
 }
 
 module.exports = printBinary;
