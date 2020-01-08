45a85309427a7ab805f522834fd29a0a5bcc6e82
Henrique Eyer <henriqueeyer@outlook.com>
2020-01-08 15:11:29 -0300

    Ajustando o average

--- tests/average.spec.js
+++ tests/average.spec.js
@@ -9,7 +9,15 @@ describe('#average', () => {
     expect(average(3, 4, 5)).toBe(4);
   });
 
-  it("expect average of 3, 'ola', 5 should be 4", () => {
+  it('expect average of 3, 4, 5, 6, 7, 8 should be 5,5', () => {
+    expect(average(3, 4, 5, 6, 7, 8)).toBe(5.5);
+  });
+
+  it('expect average of 10, 20 should be 15', () => {
+    expect(average(10, 20)).toBe(15);
+  });
+
+  it("expect average of 3, 'ola', 5 should be undefined", () => {
     expect(average(3, 'ola', 5)).toBe(undefined);
   });
 });
