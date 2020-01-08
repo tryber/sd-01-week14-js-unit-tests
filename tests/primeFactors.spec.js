af0e8f97cc306b887fdf54f3695c1e43645742e1
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2020-01-08 18:41:18 -0300

    Alterando teste em primeFactors

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -4,14 +4,17 @@ const assert = require('assert');
 // A função recebe um inteiro e retorna a lista com os fatores primos desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
 
-describe('#primeFactors', () => {
-  it('factors of numbers', () => {
-    expect(primeFactors(3)).toStrictEqual([3]);
-  });
-});
 
-assert.throws(() => { primeFactors(3.14); }, /^Error: you must send a integer/);
-assert.deepStrictEqual(primeFactors(3), [3]);
-assert.deepStrictEqual(primeFactors(6), [2, 3]);
-assert.deepStrictEqual(primeFactors(12), [2, 2, 3]);
-assert.deepStrictEqual(primeFactors(60), [2, 2, 3, 5]);
+
+describe('#primeFactors', () => {
+  it('if appear a error, the message (you must send a integer) must appear', () => {
+    assert.throws(() => { primeFactors(3.14); }, /^Error: you must send a integer/);
+  });
+
+  it('factors of numbers', () => {
+    assert.deepStrictEqual(primeFactors(3), [3]);
+    assert.deepStrictEqual(primeFactors(6), [2, 3]);
+    assert.deepStrictEqual(primeFactors(12), [2, 2, 3]);
+    assert.deepStrictEqual(primeFactors(60), [2, 2, 3, 5]);
+  })
+});
