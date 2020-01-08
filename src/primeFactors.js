62f84e6b871242bba766b92d40ece92ad9c41107
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-01-08 13:22:30 -0300

    Fix description, add examples

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -1,6 +1,16 @@
 /*
   A função primeFactors recebe um número inteiro ${number}
-  e retorna uma lista com todos os fatores desse parâmetro
+  e retorna uma lista com todos os fatores primos desse parâmetro
+
+  Por exemplo:
+
+  - os fatores primos para o número 15 são: [3, 5], pois 3 * 5 = 15
+  - os fatores primos para o número 9 são: [3, 3], pois 3 * 3 = 9
+  - os fatores primos para o número 12 são: [2, 2, 3], pois 2 * 2 * 3 = 12
+
+  Para mais informações acerca de fatores primos, acesse esta página:
+    -> https://pt.wikipedia.org/wiki/Fator_primo
+
 */
 
 function primeFactors(number) {}
