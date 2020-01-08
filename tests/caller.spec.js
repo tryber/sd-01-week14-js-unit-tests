0bb21abf6221bd16280768effd6dd4e7a02fda11
Henrique Eyer <henriqueeyer@outlook.com>
2020-01-08 15:38:53 -0300

    Resolvendo o caller

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -6,19 +6,18 @@ const caller = require('../src/caller');
 // A função deve executar a função (primeiro parâmetro) com os outros parâmetros recebidos
 // e retornar o resultado
 
-const add = jest.fn((a, b) => a + b);
-
+const func = jest.fn((a, b) => a + b);
 
 describe('#caller', () => {
   it('A soma precisa resultar em 5, 2+3', () => {
-    expect(caller(add, 2, 3)).toBe(5);
-    expect(add).toBeCalledWith(2, 3);
-    expect(add).toHaveBeenCalledTimes(1);
+    expect(caller(func, 2, 3)).toBe(5);
+    expect(func).toBeCalledWith(2, 3);
+    expect(func).toHaveBeenCalledTimes(1);
   });
 
   it('os parametros sao tutus e ronaldo, precisa sair tutusronaldo', () => {
-    expect(caller(add, 'tutus', 'ronaldo')).toBe('tutusronaldo');
-    expect(add).toBeCalledWith('tutus', 'ronaldo');
+    expect(caller(func, 'tutus', 'ronaldo')).toBe('tutusronaldo');
+    expect(func).toBeCalledWith('tutus', 'ronaldo');
   });
 });
 
