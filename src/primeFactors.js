438f77936792c39a1456b85212d1cc2ec86f6b2a
Inacio Cerqueira <inaciogcerqueira@gmail.com>
2020-01-08 13:54:00 -0300

    Make description more explicit

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -5,8 +5,8 @@
   Por exemplo:
 
   - os fatores primos para o número 15 são: [3, 5], pois 3 * 5 = 15 e ambos 3 e 5 são números primos
-  - os fatores primos para o número 9 são: [3, 3], pois 3 * 3 = 9
-  - os fatores primos para o número 12 são: [2, 2, 3], pois 2 * 2 * 3 = 12
+  - os fatores primos para o número 9 são: [3, 3], pois 3 * 3 = 9 e 3, que aparece duas vezes, é um número primo
+  - os fatores primos para o número 12 são: [2, 2, 3], pois 2 * 2 * 3 = 12 e 2, que aparece duas vezes, e 3 são números primos
 
   Para mais informações acerca de fatores primos, acesse esta página:
     -> https://pt.wikipedia.org/wiki/Fator_primo
