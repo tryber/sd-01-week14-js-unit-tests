c0f87d016caff21e122c9eb2cd4afe5fe86ef7e4
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-01-09 13:53:19 -0300

    Comentado todo o arquivo primefactors.

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -1,15 +1,15 @@
-const primeFactors = require('../src/primeFactors');
-const assert = require('assert');
+// const primeFactors = require('../src/primeFactors');
+// const assert = require('assert');
 
-// A função recebe um inteiro e retorna a lista com os fatores primos desse número
-// Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
-describe('#primeFactors', () => {
-  it('expect primeFactors is number interger', () => {
-    expect(primeFactors('Douglas')).toBe('this is not a number interger');
-  });
-});
+// // A função recebe um inteiro e retorna a lista com os fatores primos desse número
+// // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
+// describe('#primeFactors', () => {
+//   it('expect primeFactors is number interger', () => {
+//     expect(primeFactors('Douglas')).toBe('this is not a number interger');
+//   });
+// });
 
-assert.deepStrictEqual(primeFactors(15), [3, 5]);
-assert.deepStrictEqual(primeFactors(9), [3, 3]);
-assert.deepStrictEqual(primeFactors(12), [2, 2, 3]);
-assert.deepStrictEqual(primeFactors(5.5), 'this is not a number interger');
+// assert.deepStrictEqual(primeFactors(15), [3, 5]);
+// assert.deepStrictEqual(primeFactors(9), [3, 3]);
+// assert.deepStrictEqual(primeFactors(12), [2, 2, 3]);
+// assert.deepStrictEqual(primeFactors(5.5), 'this is not a number interger');
