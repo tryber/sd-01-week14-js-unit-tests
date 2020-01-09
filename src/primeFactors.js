3164d06c0b25056e7fb4f8da7bb090577e3db869
dougfunny1983 <douglaseduardo220483@gmail.com>
2020-01-09 13:54:09 -0300

    Não conseguir retirar os problemas do CC

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -1,47 +1,47 @@
-/*
-  A função primeFactors recebe um número inteiro ${number}
-  e retorna uma lista com todos os fatores primos desse parâmetro
+// /*
+//   A função primeFactors recebe um número inteiro ${number}
+//   e retorna uma lista com todos os fatores primos desse parâmetro
 
-  Por exemplo:
+//   Por exemplo:
 
-  - os fatores primos para o número 15 são [3, 5], pois:
-    1) 3 * 5 = 15
-    2) ambos 3 e 5 são números primos
-  - os fatores primos para o número 9 são [3, 3], pois:
-    1) 3 * 3 = 9
-    2) 3, que aparece duas vezes, é um número primo
-  - os fatores primos para o número 12 são [2, 2, 3], pois:
-    1) 2 * 2 * 3 = 12
-    2) 2, que aparece duas vezes, e 3 são números primos
+//   - os fatores primos para o número 15 são [3, 5], pois:
+//     1) 3 * 5 = 15
+//     2) ambos 3 e 5 são números primos
+//   - os fatores primos para o número 9 são [3, 3], pois:
+//     1) 3 * 3 = 9
+//     2) 3, que aparece duas vezes, é um número primo
+//   - os fatores primos para o número 12 são [2, 2, 3], pois:
+//     1) 2 * 2 * 3 = 12
+//     2) 2, que aparece duas vezes, e 3 são números primos
 
-  Para mais informações acerca de fatores primos, acesse esta página:
-    -> https://pt.wikipedia.org/wiki/Fator_primo
-*/
+//   Para mais informações acerca de fatores primos, acesse esta página:
+//     -> https://pt.wikipedia.org/wiki/Fator_primo
+// */
 
-const verificarSePrimoTrue = (numeroASeColocar) => {
-  let inicial = 2;
-  while (inicial <= Math.sqrt(numeroASeColocar))
-    if (numeroASeColocar % (inicial += 1) < 1) return false;
-  return numeroASeColocar > 1;
-};
+// const verificarSePrimoTrue = (numeroASeColocar) => {
+//   let inicial = 2;
+//   while (inicial <= Math.sqrt(numeroASeColocar))
+//     if (numeroASeColocar % (inicial += 1) < 1) return false;
+//   return numeroASeColocar > 1;
+// };
 
-const primeFactors = (number) => {
-  const lista = [];
-  let value = number;
-  if (!Number.isInteger(value)) {
-    return 'this is not a number interger';
-  } else {
-    for (let index = 0; index < number; index += 1) {
-      let valor = index;
-      if (verificarSePrimoTrue(index)) {
-        while (value % valor === 0) {
-          lista.push(index);
-          value = value / index;
-        }
-      }
-    }
-    return lista;
-  }
-};
+// const primeFactors = (number) => {
+//   const lista = [];
+//   let value = number;
+//   if (!Number.isInteger(value)) {
+//     return 'this is not a number interger';
+//   } else {
+//     for (let index = 0; index < number; index += 1) {
+//       let valor = index;
+//       if (verificarSePrimoTrue(index)) {
+//         while (value % valor === 0) {
+//           lista.push(index);
+//           value = value / index;
+//         }
+//       }
+//     }
+//     return lista;
+//   }
+// };
 
-module.exports = primeFactors;
+// module.exports = primeFactors;
