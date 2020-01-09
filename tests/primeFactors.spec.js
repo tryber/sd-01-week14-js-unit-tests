339cb1cec29266860d8361c4c8dfbd8476d4c817
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-01-09 12:28:27 -0300

    consertando o jest

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -3,11 +3,11 @@ const assert = require('assert');
 
 // A função recebe um inteiro e retorna a lista com os fatores primos desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
-// describe('#primeFactors', () => {
-//   it('expect primeFactors is number interger', () => {
-//     expect(primeFactors('Douglas')).toBe('this is not a number');
-//   });
-// });
+describe('#primeFactors', () => {
+  it('expect primeFactors is number interger', () => {
+    expect(primeFactors('Douglas')).toBe('this is not a number interger');
+  });
+});
 
 assert.deepStrictEqual(primeFactors(15), [3, 5]);
 assert.deepStrictEqual(primeFactors(9), [3, 3]);
