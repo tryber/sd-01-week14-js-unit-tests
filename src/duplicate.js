c4e66c883cfdba88384ebf1bc75b182783938b06
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-01-09 14:04:04 -0300

    codeclimate

--- src/duplicate.js
+++ src/duplicate.js
@@ -3,10 +3,11 @@
   e retorna `true` se existe algum elemento duplicado e `false` caso contrário
  */
 
-function duplicate(params) {
-  for (let i = 0; i < params.length; i += 1) {
-    return params.every(element => element === params[i]);
-  }
+function duplicate(...params) {
+  const set = new Set(params);
+  const set1 = set.size;
+  if (set1 === params.length) return false;
+  return true;
 }
 
 module.exports = duplicate;
