9058675a065a58939fec681131d6ff70e8faec78
Guilherme Luiz <guiiluiz44@gmail.com>
2020-01-09 11:34:03 -0300

    finaliza testes da funcao primeFactors

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -3,3 +3,25 @@ const assert = require('assert');
 
 // A função recebe um inteiro e retorna a lista com os fatores primos desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
+
+describe('#primeFactors', () => {
+  it('should print the prime factor numbers of the integer 3', () => {
+    expect(primeFactors(3)).toStrictEqual([3]);
+  });
+
+  it('should print the prime factor numbers of the integer 6', () => {
+    expect(primeFactors(6)).toStrictEqual([2, 3]);
+  });
+
+  it('should print the prime factor numbers of the integer 9', () => {
+    expect(primeFactors(9)).toStrictEqual([3, 3]);
+  });
+
+  it('should print the prime factor numbers of the integer 60', () => {
+    expect(primeFactors(60)).toStrictEqual([2, 2, 3, 5]);
+  });
+
+  it('throw Error Message if do not receive an integer', () => {
+    expect(primeFactors).toThrow('You must pass an integer!');
+  });
+});
