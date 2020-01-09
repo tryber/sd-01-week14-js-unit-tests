8ac807346f07eba1e8c56f68a7b28bc24ba7d376
Victor Felipe <vf.ramos7@gmail.com>
2020-01-09 11:16:42 -0300

    Finalizando o teste primeFactory

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -3,6 +3,16 @@
   e retorna uma lista com todos os fatores desse parâmetro
 */
 
-function primeFactors(number) {}
+function primeFactors(number) {
+  const lista = [];
+  let value = number;
+
+  if (!Number.isInteger(number)) throw new Error('Não é um número.')
+
+  for (let i = 1; i < number; i += 1) {
+    lista.push((value *= i));
+  }
+  return lista;
+}
 
 module.exports = primeFactors;
--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -3,3 +3,12 @@ const assert = require('assert');
 
 // A função recebe um inteiro e retorna a lista com os fatores desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
+
+assert.deepEqual(primeFactors(5), [20, 60, 120, 120]);
+assert.throws(() => primeFactors(2.1), /Não é um número inteiro!/);
+
+describe('#primeFactors', () => {
+    it('expect prime factor 3',() => {
+        expect(primeFactors(3)).toStrictEqual([6, 6]);
+    });
+});
