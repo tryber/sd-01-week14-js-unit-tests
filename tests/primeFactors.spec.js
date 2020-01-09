c1450714c4c47c545e256b9da9fc357219dcdf51
Conrado Medeiros <conradodemedeiros@hotmail.com>
2020-01-09 14:11:55 -0300

    codeclimate

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 // A função recebe um inteiro e retorna a lista com os fatores desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
 
-describe("#primeFactors", () => {
+describe('#primeFactors', () => {
   it('if appear a error, the message (pass integer number) must appear', () => {
     assert.throws(() => {
       primeFactors(3.14);
