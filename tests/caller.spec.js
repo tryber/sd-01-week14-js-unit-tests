072470c0def56d785fcf633983b992a0cc8df1a2
Victor Felipe <vf.ramos7@gmail.com>
2020-01-09 10:40:15 -0300

    Finalizado o teste Caller

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -5,3 +5,14 @@ const caller = require('../src/caller');
 // O segundo e terceiro parâmetros podem ser de qualquer tipo
 // A função deve executar a função (primeiro parâmetro) com os outros parâmetros recebidos
 // e retornar o resultado
+
+const fn = (a, b) => a * b;
+
+describe('#testCaller', () => {
+    it('Caller', () => {
+        expect(caller(fn, 3, 4)).toBe(12);
+    });
+    it('Caller with string', () => {
+        expect(caller(fn, 3, '4')).toBe(12);
+    });
+});
