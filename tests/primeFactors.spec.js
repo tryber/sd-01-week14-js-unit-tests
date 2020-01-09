116765c97bdc05f3aa9541bd5d3a73785c479cad
Victor Felipe <vf.ramos7@gmail.com>
2020-01-09 13:56:43 -0300

    cc

--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -8,7 +8,7 @@ assert.deepEqual(primeFactors(5), [20, 60, 120, 120]);
 assert.throws(() => primeFactors(2.1), /Não é um número inteiro!/);
 
 describe('#primeFactors', () => {
-    it('expect prime factor 3', () => {
-        expect(primeFactors(3)).toStrictEqual([6, 6]);
-    });
+  it('expect prime factor 3', () => {
+    expect(primeFactors(3)).toStrictEqual([6, 6]);
+  });
 });
