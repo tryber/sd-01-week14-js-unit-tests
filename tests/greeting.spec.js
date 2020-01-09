64cd0253286f63b581904466470283b87b94e120
Victor Felipe <vf.ramos7@gmail.com>
2020-01-09 10:12:20 -0300

    Finalizado o teste greeting

--- tests/greeting.spec.js
+++ tests/greeting.spec.js
@@ -3,3 +3,12 @@ const greeting = require('../src/greeting');
 // A função deve receber uma string ${param} e retornar a mensagem 'Hello ${param}!'
 // Se nenhum parâmetro for passado um erro deve ser lançado
 // A mensagem do erro deve ser 'You must send something to greet!'
+
+describe('#greeting', () => {
+    it('Função recebe uma String e retorna a mensagem.', () => {
+        expect(greeting('Stomatopoda')).toBe('Hello Stomatopoda!');
+    });
+    it('Nenhum parâmetro passado.', () => {
+        expect(greeting).toThrow('You must send something to greet!');
+    });
+});
