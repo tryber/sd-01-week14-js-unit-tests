b5aba7753aed69bc47dae5ad9651dc8ba4b383a3
Douglas Eduardo <douglaseduardo220483@gmail.com>
2020-01-09 10:46:10 -0300

    consertando o Caller

--- tests/caller.spec.js
+++ tests/caller.spec.js
@@ -8,6 +8,17 @@ const caller = require('../src/caller');
 const fn = (a, b) => a * b;
 
 describe('#caller', () => {
+  it('Verificar se a função está ok', () => {
+    const multiplicação = jest.fn();
+    caller(multiplicação, 1, 2);
+    expect(multiplicação).toBeCalledWith(1, 2);
+    expect(multiplicação).toHaveBeenCalled();
+  });
+
+  it('The fn is a function', () => {
+    expect(typeof fn).toBe('function');
+  });
+
   it('expect caller of fn, 4, 5 should be fn(4,5)', () => {
     expect(caller(fn, 2, 5)).toBe(10);
   });
