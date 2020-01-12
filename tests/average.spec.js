9196ac73a7e2c23151f93c845a05e12e91af9413
antoniopaolillo <antonio_francisco_p@live.com>
2020-01-12 18:15:25 -0300

    adicionando casos de teste no arquivo 'average'

--- tests/average.spec.js
+++ tests/average.spec.js
@@ -5,10 +5,19 @@ const average = require('../src/average');
 // Caso a função receba algum valor não número, o valor undefined deve ser retornado
 
 describe('#average', () => {
+  it('expect average of 1 should be 1', () => {
+    expect(average(1)).toBe(1);
+  });
+  it('expect average of 6 and 8 should be 7', () => {
+    expect(average(6, 8)).toBe(7);
+  });
   it('expect average of 3, 4, 5 should be 4', () => {
     expect(average(3, 4, 5)).toBe(4);
   });
   it('expect average of string should be undefined', () => {
     expect(average(3, 4, 'something')).toBe(undefined);
+  });
+  it('expect average of string should be undefined', () => {
+    expect(average('word', 'something')).toBe(undefined);
   });
 });
