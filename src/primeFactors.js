9a6faae6d511c5e8b38f21aa43da7ef277ebc76a
antoniopaolillo <antonio_francisco_p@live.com>
2020-01-13 10:01:34 -0300

    arrumando arquivo primeFactors

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -22,8 +22,14 @@ function primeFactors(number) {
   const array = [];
   let totalValue = number;
   if (!Number.isInteger(number)) throw new Error('Número não inteiro!');
-  for (let index = number - 1; index > 0; index -= 1) {
-    array.push(totalValue *= index);
+
+  for (let index = 2; totalValue !== 1;) {
+    if (totalValue % index === 0) {
+      array.push(index);
+      totalValue /= index;
+    } else {
+      index += 1;
+    }
   }
   return array;
 }
--- tests/primeFactors.spec.js
+++ tests/primeFactors.spec.js
@@ -4,13 +4,9 @@ const assert = require('assert');
 // A função recebe um inteiro e retorna a lista com os fatores primos desse número
 // Se o parâmetro recebido for um valor não inteiro um erro deve ser lançado
 
-assert.deepEqual(primeFactors(3), [6, 6]);
-assert.deepEqual(primeFactors(5), [20, 60, 120, 120]);
-assert.deepEqual(primeFactors(8), [56, 336, 1680, 6720, 20160, 40320, 40320]);
+assert.deepEqual(primeFactors(15), [3, 5]);
+assert.deepEqual(primeFactors(9), [3, 3]);
+assert.deepEqual(primeFactors(12), [2, 2, 3]);
+assert.deepEqual(primeFactors(1576), [2, 2, 2, 197]);
 assert.throws(() => primeFactors(5.4), /Número não inteiro!/);
 
-describe('#primeFactors', () => {
-  it('expect return [3, 2, 1] until 3', () => {
-    expect(primeFactors(3)).toStrictEqual([6, 6]);
-  });
-});
