11bc68d96eb6496e523a6155ea910ec5867b780c
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-01-13 17:16:53 -0200

    Bonus

--- src/rpnCalculator.js
+++ src/rpnCalculator.js
@@ -5,6 +5,26 @@
   referência: https://en.wikipedia.org/wiki/Reverse_Polish_notation
  */
 
-function rpnCalculator(expression) {}
+function rpnCalculator(expression) {
+  const arrayStrings = expression.split(' ').filter(element => element !== ' ');
+  const operators = ['+', '-', '/', '*'];
+  arrayStrings.forEach(element => {
+    if (isNaN(Number(element)) && !operators.includes(element)) throw new Error('The function do not support this operator or it do not exist');
+  })
+  let numbers = [];
+  let value;
+  arrayStrings.forEach(element => {
+    if (!isNaN(Number(element))) {
+      numbers.push(element);
+    } else {
+      value = eval(`${numbers[numbers.length - 2]} ${element} ${numbers[numbers.length - 1]} `);
+      numbers.splice(numbers.length - 2, 2);
+      numbers.push(value);
+    }
+  });
+  return numbers[0];
+}
 
 module.exports = rpnCalculator;
+
+
--- tests/rpnCalculator.spec.js
+++ tests/rpnCalculator.spec.js
@@ -6,3 +6,17 @@ const rpnCalculator = require('../src/rpnCalculator');
 // Qualquer outro operador deve lançar um erro
 
 // ps: utilize os exemplos da documentação (README.md) para contrinuir com os testes
+
+describe('#rpnCalculator', () => {
+  it('should return the result of the converted expression', () => {
+    expect(rpnCalculator('3 4 - 5 *')).toStrictEqual(-5);
+    expect(rpnCalculator('3 4 5 * -')).toStrictEqual(-17);
+    expect(rpnCalculator('3 4 - 5 +')).toStrictEqual(4);
+    expect(rpnCalculator('15 7 1 1 + - / 3 * 2 1 1 + + -')).toStrictEqual(5);
+    expect(rpnCalculator('3 4 +')).toStrictEqual(7);
+  });
+  it('should return a error message if the function do not support an operator', () => {
+    expect(() => rpnCalculator('3 4 ^ 5 *')).toThrow('The function do not support this operator or it do not exist');
+    expect(() => rpnCalculator('3 4 adele 5 *')).toThrow('The function do not support this operator or it do not exist');
+  });
+});
