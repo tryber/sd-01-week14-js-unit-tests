7a38cce4c1dc1c60cebca3ae485f209b2cfceb09
Mateus Talles <mateustalles@gmail.com>
2020-01-14 15:43:44 -0300

    LEssen complexity

--- src/primeFactors.js
+++ src/primeFactors.js
@@ -5,24 +5,20 @@
 
 function primeFactors(number) {
   let factors = [1, number];
-  function addToFactors(n, m) {
+  function addToFactors(n) {
     if (!factors.includes(n)) {
       factors.push(n);
+     }
     }
-    if (!factors.includes(m)) {
-      factors.push(m);
-    }
-  }
   for (let i = 2; i <= number; i++) {
     const storage = number;
     if (number % i === 0) {
-      addToFactors(number, i);
+      addToFactors(i);
       while((number/i)%i === 0) {
-        addToFactors(number, i);
+        addToFactors(i);
+        addToFactors(number);
         number = number/i;
-        addToFactors(number, i);
       }
-      addToFactors(number, i);
     }
     number = storage;
   }
