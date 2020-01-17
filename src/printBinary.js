81956fff2d62420374e7da53abb4945f29e56616
vrramos <vf.ramos7@gmail.com>
2020-01-17 17:02:57 -0300

    Consertado o requisito Print Binary

--- src/printBinary.js
+++ src/printBinary.js
@@ -3,6 +3,25 @@
   e retorna uma lista com todos os números binários existentes com a quantidade ${number} de dígitos
  */
 
-function printBinary(number) {}
+const NumerosBinariosExistentes = (number, binary) => {
+  let num = binary;
+  while (num.length < number) {
+    num = `0${num}`;
+  }
+  return num;
+};
+
+function printBinary(number) {
+  let arrayBinary = [];
+  const binaryList = [];
+  for (let i = 0; i < number; i++) {
+    arrayBinary.push('1');
+  }
+  arrayBinary = parseInt(arrayBinary.join(''), 2);
+  for (let i = 0; i <= arrayBinary; i++) {
+    binaryList.push(i);
+  }
+  return binaryList.map(decimal => NumerosBinariosExistentes(number, decimal.toString(2)));
+}
 
 module.exports = printBinary;
